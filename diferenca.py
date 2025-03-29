63ee3c331519731c65f6587f7992858c9cff4223
Battler89 <167143919+Battler89@users.noreply.github.com>
2025-03-29 12:54:50 -0300

    Create diferenca.py

---
+++ diferenca.py
@@ -0,0 +1,7 @@
+# Programa para calcular e exibir a multiplicacao do primeiro e segundo numeros menos a multiplicacao do terceiro e quatro numeros
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+c = int(input("Digite um número: "))
+d = int(input("Digite um número: "))
+diferenca = (a * b) - (c * d)
+print(f"Diferenca: {diferenca}")
