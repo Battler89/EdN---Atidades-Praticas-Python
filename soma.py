10de8f04b001c33d2750c7ec72c03cfa97618e39
Battler89 <167143919+Battler89@users.noreply.github.com>
2025-03-29 12:47:32 -0300

    Create soma.py
    Escola da Nuvem
    Atividades Práticas 1
    2- Calculadora de Soma
    Desenvolver um programa que soma dois números. 
    Use as variáveis numero1 = 12 e numero2 = 14. 
    O programa deve calcular a soma e exibir o resultado.

---
+++ soma.py
@@ -0,0 +1,11 @@
+# Programa para mostar o resultado da soma, multiplicacao, subtracao e divisao dos números 12 e 14
+numero1 = 12 
+numero2 = 14
+adic = numero1 + numero2
+mult = numero1 * numero2
+subt = numero1 - numero2
+divi = numero1 / numero2
+print(f"O número {numero1} somado ao número {numero2} é: {adic}") 
+print(f"A multiplicação de {numero1} * {numero2} é: {mult}")
+print(f"A subtração de {numero1} - {numero2} é: {subt}")
+print(f"A divisão de {numero1} / {numero2} é: {divi}")
