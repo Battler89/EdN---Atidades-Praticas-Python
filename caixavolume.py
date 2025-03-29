03de7a06b36d19afba975b5c0ac421dbfd42e9a4
Battler89 <167143919+Battler89@users.noreply.github.com>
2025-03-29 12:38:00 -0300

    Create caixavolume.py
    Escola da Nuvem
    Atividades Práticas 1
    3- Calculadora de Volume
    * Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
    * Comprimento: 12 cm
    * Largura: 14 cm
    * Altura: 20 cm
    O programa deve calcular o volume e exibir o resultado em cm³.

---
+++ caixavolume.py
@@ -0,0 +1,6 @@
+# Realizar o calculo do volume de uma caixa retangular
+comprimento = 12
+largura = 14
+altura = 20
+volume_retangulo = comprimento * largura * altura
+print(f"O volume da caixa retangular é de: {volume_retangulo} cm³")
