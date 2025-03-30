1b4b64a554521eae94fbefe4b226c6d55e857864
Battler89 <167143919+Battler89@users.noreply.github.com>
2025-03-30 13:42:45 -0300

    Create verificasenha.py

---
+++ verificasenha.py
@@ -0,0 +1,17 @@
+# Programa para verificar se a senha inserida atende aos requisitos
+def verifica_senha():
+    while True:
+        senha = input("Digite uma senha (ou 'sair' para encerrar): ")
+        print("A senha deve conter no minimo 8 caracteres e ao menos um número!")
+        if senha.lower() == 'sair':
+            print("Programa encerrado.")
+            break
+        if len(senha) < 8:
+            print("Senha fraca: deve ter pelo menos 8 caracteres.")
+            continue
+        if not any(caractere.isdigit() for caractere in senha):
+            print("Senha fraca: deve conter pelo menos um número.")
+            continue
+        print("Senha forte e válida!")
+        break
+verifica_senha()
